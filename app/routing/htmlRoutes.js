78460b864c62de73ae18484607b6d9754ef5e9b5
Tom Dolphin <thedolphingroup1@gmail.com>
2018-01-23 16:16:54 -0700

    updated routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,15 +2,15 @@ var path = require("path");
 
 module.exports = function (app) {
 
-	app.get('/', function(req,res) {
+	app.use('/', function(req,res) {
 		res.sendFile(path.join(__dirname + '/../public/index.html'));
 	});
 
-	app.get('/contact', function(req,res) {
+	app.use('/contact', function(req,res) {
 		res.sendFile(path.join(__dirname + '/../public/contact.html'));
     });
     
-    app.get('/portfolio', function(req,res) {
+    app.use('/portfolio', function(req,res) {
 		res.sendFile(path.join(__dirname + '/../public/portfolio.html'));
 	});
 }
