55e7af6a1fe2865dfed8c2ac7b7fc5bd49513487
Tom Dolphin <thedolphingroup1@gmail.com>
2018-01-23 16:18:27 -0700

    redid routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,15 +2,15 @@ var path = require("path");
 
 module.exports = function (app) {
 
-	app.use('/', function(req,res) {
+	app.get('/', function(req,res) {
 		res.sendFile(path.join(__dirname + '/../public/index.html'));
 	});
 
-	app.use('/contact', function(req,res) {
+	app.get('/contact', function(req,res) {
 		res.sendFile(path.join(__dirname + '/../public/contact.html'));
     });
     
-    app.use('/portfolio', function(req,res) {
+    app.get('/portfolio', function(req,res) {
 		res.sendFile(path.join(__dirname + '/../public/portfolio.html'));
 	});
 }
