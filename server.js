a295bc5a1f191934615d89616193bc193ee29c36
Tom Dolphin <thedolphingroup1@gmail.com>
2018-01-23 17:20:57 -0700

    updated server.js

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var express = require('express')
 var app = express()
  
 var PORT = process.env.PORT || 8080;
- 
 
+app.use(express.static('public'))
+ 
 //require routes for routes
 require("./app/routing/htmlRoutes.js")(app);
  
